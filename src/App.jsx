7556de56a4fe67e8b0fc90483e3b91f79b535f2f
Mateo Lohezic <mateolohezic@gmail.com>
2023-11-28 12:01:34 -0300

    New: Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import './App.css'
 import { Route, Routes } from "react-router-dom";
 import Home from './Pages/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
 
   return (
     <>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home/>} />
       </Routes>
