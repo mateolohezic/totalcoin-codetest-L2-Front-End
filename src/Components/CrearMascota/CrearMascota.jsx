4e20dc456da39c8786a08c9a79dfcb8985e00a3e
Mateo Lohezic <mateolohezic@gmail.com>
2023-12-01 08:16:18 -0300

    New: Crear mascota

---
+++ src/Components/CrearMascota/CrearMascota.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './crearMascota.css'
+
+function CrearMascota() {
+    return (
+    <>
+      
+    </>
+    )
+}
+
+export default CrearMascota
--- src/Pages/Mascotas/Mascotas.jsx
+++ src/Pages/Mascotas/Mascotas.jsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './mascotas.css'
 import GrillaMisMascotas from '../../Components/GrillaMisMascotas/GrillaMisMascotas'
+import CrearMascota from '../../Components/CrearMascota/CrearMascota'
 
 function Mascotas() {
+
+    const [creating, setCreating] = useState(false)
+
     return (
     <>
         <main>
-            <section className='contenedorMascotas'>
-                <h1>Mis Mascotas</h1>
-                <p>Desde este menú puedes administrar todas mascotas y realizar pedidos.</p>
-            </section>
-            <GrillaMisMascotas/>
+            {
+                !creating ? 
+                <>
+                    <section className='contenedorMascotas'>
+                        <h1>Mis Mascotas</h1>
+                        <p>Desde este menú puedes administrar todas tus mascotas y realizar pedidos.</p>
+                    </section>
+                    <section className='contenedorCrearMascota'>
+                        <div className='botonAñadirCrearMascota'>
+                            <button type="button" onClick={ () => { setCreating(true) } }>+ Añadir</button>
+                        </div>
+                    </section>
+                    <GrillaMisMascotas/>
+                </>
+                :
+                <>
+                    <CrearMascota/>
+                </>
+            }
         </main>
     </>
     )
